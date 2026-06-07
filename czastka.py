473c89cbb77ec888e2e6e7dfe0b92b7979caca1d
szymonskrzypniak <szymonskrzypniak.wp.pl@gmail.com>
2026-06-07 22:05:16 +0200

    Refactor Czastka class for velocity and position updates
    w gazie doskonałym możemy olać promień cząstek to to pozmieniałem, dodałem krok czasowy w updatowaniu pozycji z prędkości i przerobiłem też ten mechanizm odbicia, środek pojemnika jest w (0,0)

--- czastka.py
+++ czastka.py
@@ -1,12 +1,11 @@
 import random
 import math
+import numpy as np
 
 class Czastka:
-    def __init__(self, x, y, promien=5, masa=1):
+    def __init__(self, x, y, masa=1, dt):
         self.x = x
         self.y = y
-
-        self.promien = promien
         self.masa = masa
 
         self.vx = random.uniform(-3, 3)
@@ -14,10 +13,12 @@ class Czastka:
 
 
     def ruch(self):
-        self.x += self.vx
-        self.y += self.vy
+        self.x += self.vx*dt
+        self.y += self.vy*dt
 
     def czy_sciana(self, dlugosc_podstawy, wysokosc_sciany):
-        if self.x - self.radius < 0:
-            self.x = self.radius
+        if np.absolute(self.x) > dlugosc_podstawy/2:
             self.vx *= -1
+        if np.absolute(self.y) > dlugosc_podstawy/2:
+            self.vy *= -1
+            
